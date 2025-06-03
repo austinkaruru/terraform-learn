767123500f9cbcb6a517499c1c8bde915b820e02
austinkaruru <karuruaustine@gmail.com>
2025-06-03 08:04:27 +0300

    added terraform code for vpc, subnet, availability zone, security group, route table & internet gateway

--- main.tf
+++ main.tf
@@ -1,40 +1,104 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
 provider "aws" {
     region = "eu-north-1"
-
+    
 }
 
 
-variable "cidr_blocks" {
-    description = "cidr blocks & name tags for vpc & subnets"
-    type = list(object({
-        cidr_block = string
-        name = string
-    }))
-}
-
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
 variable avail_zone {}
+variable env_prefix {}
+variable my-ip {}
 
-resource "aws_vpc" "dev-vpc" {
-    cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = var.vpc_cidr_block
     tags = {
-      Name: var.cidr_blocks[0].name
+      Name: "${var.env_prefix}-vpc"
     }
 }
 
-resource "aws_subnet" "dev-vpc-subnet" {
-    vpc_id = aws_vpc.dev-vpc.id
-    cidr_block = var.cidr_blocks[1].cidr_block
+resource "aws_subnet" "myapp-subnet-1" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
-        Name = var.cidr_blocks[1].name
+        Name = "${var.env_prefix}-subnet-1"
     }
 }
 
-output "dev-vpc-id" {
-    value = aws_vpc.dev-vpc.id
+# resource "aws_route_table" "myapp-route-table" {
+#     vpc_id = aws_vpc.myapp-vpc.id
 
+#     route {
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.myapp-igw.id
+#     }
+
+#     tags = {
+#         Name = "${var.env_prefix}-rtb"
+#     }
+# }
+
+resource "aws_internet_gateway" "myapp-igw"{
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+        Name = "${var.env_prefix}-igw"
+    }
 }
 
-output "dev-vpc-subnet-id" {
-    value = aws_subnet.dev-vpc-subnet.id
+resource "aws_default_route_table" "default-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+    tags = {
+        Name = "${var.env_prefix}-main-rtb"
+    }
+}
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#     subnet_id = aws_subnet.myapp-subnet-1.id
+#     route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my-ip] 
+
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name = "${var.env_prefix}-default-sg"
+    }
 }
