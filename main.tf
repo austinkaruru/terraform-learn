3b31e372eedd84bf5d75630c189e7bf4646a12fc
austinkaruru <karuruaustine@gmail.com>
2025-06-05 06:20:23 +0300

    added resource blocks for ec2 instance, key pair, public ip, amazon machine image

--- main.tf
+++ main.tf
@@ -18,6 +18,9 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my-ip {}
+variable instance_type {} 
+variable public_key {}
+  
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -101,4 +104,51 @@ resource "aws_default_security_group" "default-sg" {
     tags = {
         Name = "${var.env_prefix}-default-sg"
     }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+    description = "The ID of the latest Amazon Linux AMI"
+  
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+    description = "The public IP address of the EC2 instance"
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = var.public_key
+}
+
+resource "aws_instance" "myapp-server" {
+
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+    tags = {
+        Name = "${var.env_prefix}-server"
+    } 
+
+  
 }
